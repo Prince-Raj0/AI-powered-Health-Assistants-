0f77c49420134c7c5ca183ab531c63928d6b23cc
Prince <prince825101@gmail.com>
2025-02-10 14:40:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-#Download necessary NLTK data
+# Download necessary NLTK data
 nltk.download('punkt')
 nltk.download('stopwords')
 
-#Load a pre-trained Hugging Face model
-chatbot = pipeline("text-generation", model="deepseek-ai/DeepSeek-R1", trust_remote_code=True)
 
-#Define healthcare-specific response logic (or use a model to generate responses)
+# Load a pre-trained Hugging Face model
+chatbot = pipeline("text-generation", model="distilgpt2")
+
+
+# Define healthcare-specific response logic (or use a model to generate responses)
 def healthcare_chatbot(user_input):
     # Simple rule-based keywords to respond
     if "symptom" in user_input:
@@ -22,27 +24,28 @@ def healthcare_chatbot(user_input):
         return "It's important to take your prescribed medications regularly. If you have concerns, consult your doctor."
     else:
         # For other inputs, use the Hugging Face model to generate a response
-        response = chatbot(user_input, max_length=500, num_return_sequences=1)
+        response = chatbot(user_input, max_length=300, num_return_sequences=1)
+        # Specifies the maximum length of the generated text response, including the input and the generated tokens.
+        # If set to 3, the model generates three different possible responses based on the input.
         return response[0]['generated_text']
 
-#Streamlit web app interface
+
+# Streamlit web app interface
 def main():
     # Set up the web app title and input area
     st.title("Healthcare Assistant Chatbot")
-
+    
     # Display a simple text input for user queries
     user_input = st.text_input("How can I assist you today?", "")
-
+    
     # Display chatbot response
     if st.button("Submit"):
         if user_input:
             st.write("User: ", user_input)
-            with st.spinner("Processing your query, please wait..."):
-                response = healthcare_chatbot(user_input)
-            print("response ", response)
-            print("=======================================================")
+            response = healthcare_chatbot(user_input)
             st.write("Healthcare Assistant: ", response)
         else:
             st.write("Please enter a query.")
+
 if __name__ == "__main__":
     main()

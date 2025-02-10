3ec41d988594a40ad142353a99fa8a5e8a49c386
Prince <prince825101@gmail.com>
2025-02-10 17:21:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,76 +1,42 @@
+import os
 import streamlit as st
-from streamlit_chat import message
-from langchain.chains import ConversationalRetrievalChain
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import CTransformers
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.memory import ConversationBufferMemory
+import requests
+from dotenv import find_dotenv, load_dotenv
 
-#load the pdf files from the path
-loader = DirectoryLoader('data/',glob="*.pdf",loader_cls=PyPDFLoader)
-documents = loader.load()
+load_dotenv(find_dotenv())
+HUGGINGFACE_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
+headers = {"Authorization": f"Bearer {HUGGINGFACE_API_TOKEN}"}
 
-#split text into chunks
-text_splitter  = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
-text_chunks = text_splitter.split_documents(documents)
+def generate_response(prompt):
+    API_URL = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
 
-#create embeddings
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                   model_kwargs={'device':"cpu"})
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+        
+    output = query({
+        "inputs": f"{prompt}",
+    })
 
-#vectorstore
-vector_store = FAISS.from_documents(text_chunks,embeddings)
+    try:
+        generated_text = output[0]['generated_text']
+    except KeyError:
+        print('KeyError encountered when accessing "generated_text"')
+        generated_text = 'KeyError encountered when accessing "generated_text"'
+    
+    return generated_text.replace(prompt, "").strip()
 
-#create llm
-llm = CTransformers(model="llama-2-7b-chat.ggmlv3.q4_0.bin",model_type="llama",
-                    config={'max_new_tokens':128,'temperature':0.01})
 
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+def main():
+    #Streamlit
+    st.set_page_config(page_title="AI Powered Health Assistant", page_icon='🤖')
+    st.header('AI ChatBot')
+    messages = st.container(height=400)
+    if prompt := st.chat_input("Say something"):
+        messages.chat_message("user").write(prompt)
+        response = generate_response(prompt)
+        messages.chat_message("assistant").write(f"AiChat: {response}")
 
-chain = ConversationalRetrievalChain.from_llm(llm=llm,chain_type='stuff',
-                                              retriever=vector_store.as_retriever(search_kwargs={"k":2}),
-                                              memory=memory)
 
-st.title("HealthCare ChatBot 🧑🏽‍⚕️")
-def conversation_chat(query):
-    result = chain({"question": query, "chat_history": st.session_state['history']})
-    st.session_state['history'].append((query, result["answer"]))
-    return result["answer"]
-
-def initialize_session_state():
-    if 'history' not in st.session_state:
-        st.session_state['history'] = []
-
-    if 'generated' not in st.session_state:
-        st.session_state['generated'] = ["Hello! Ask me anything about 🤗"]
-
-    if 'past' not in st.session_state:
-        st.session_state['past'] = ["Hey! 👋"]
-
-def display_chat_history():
-    reply_container = st.container()
-    container = st.container()
-
-    with container:
-        with st.form(key='my_form', clear_on_submit=True):
-            user_input = st.text_input("Question:", placeholder="Ask about your Mental Health", key='input')
-            submit_button = st.form_submit_button(label='Send')
-
-        if submit_button and user_input:
-            output = conversation_chat(user_input)
-
-            st.session_state['past'].append(user_input)
-            st.session_state['generated'].append(output)
-
-    if st.session_state['generated']:
-        with reply_container:
-            for i in range(len(st.session_state['generated'])):
-                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
-                message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
-
-# Initialize session state
-initialize_session_state()
-# Display chat history
-display_chat_history()
+if __name__ == '__main__':
+    main()

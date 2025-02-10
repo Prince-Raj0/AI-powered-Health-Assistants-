7a07f2b286b5175957f183890d45b65776e01504
Prince <prince825101@gmail.com>
2025-02-10 12:30:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# Download necessary NLTK data
+Download necessary NLTK data
 nltk.download('punkt')
 nltk.download('stopwords')
 
-
-# Load a pre-trained Hugging Face model
+Load a pre-trained Hugging Face model
 chatbot = pipeline("text-generation", model="deepseek-ai/DeepSeek-R1", trust_remote_code=True)
-pipe(messages)
 
-
-# Define healthcare-specific response logic (or use a model to generate responses)
+Define healthcare-specific response logic (or use a model to generate responses)
 def healthcare_chatbot(user_input):
     # Simple rule-based keywords to respond
     if "symptom" in user_input:
@@ -26,27 +23,23 @@ def healthcare_chatbot(user_input):
     else:
         # For other inputs, use the Hugging Face model to generate a response
         response = chatbot(user_input, max_length=500, num_return_sequences=1)
-        # Specifies the maximum length of the generated text response, including the input and the generated tokens.
-        # If set to 3, the model generates three different possible responses based on the input.
         return response[0]['generated_text']
 
-
-# Streamlit web app interface
+Streamlit web app interface
 def main():
     # Set up the web app title and input area
     st.title("Healthcare Assistant Chatbot")
-    
+
     # Display a simple text input for user queries
     user_input = st.text_input("How can I assist you today?", "")
-    
+
     # Display chatbot response
     if st.button("Submit"):
         if user_input:
             st.write("User: ", user_input)
-            # response = healthcare_chatbot(user_input)
             with st.spinner("Processing your query, please wait..."):
                 response = healthcare_chatbot(user_input)
-            print("response ",response)
+            print("response ", response)
             print("=======================================================")
             st.write("Healthcare Assistant: ", response)
         else:
